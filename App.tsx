7489b4ea02351de87a07e967407bfec59032ec10
Willian-Rodrigues <willian.rsilva1994@gmail.com>
2022-07-25 12:06:17 -0300

    implements splashscreen and app icon

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 import { Footer } from './src/components/display/footer/footer.component';
-import AppLoading from 'expo-app-loading';
-import React, { useEffect } from 'react';
-import { StatusBar } from 'react-native';
+import React, { useCallback, useEffect } from 'react';
+import { StatusBar, View } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/global/styles/theme';
 import { useApp } from './src/hooks/app.hook';
 import { Home } from './src/screens/home/home.screen';
+import { hideAsync } from 'expo-splash-screen';
 
 export const App: React.FC = () => {
   const { getStoredTheme, fontsLoaded, selectedTheme } = useApp();
@@ -16,8 +16,14 @@ export const App: React.FC = () => {
     getStoredTheme();
   }, [getStoredTheme]);
 
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await hideAsync();
+    }
+  }, [fontsLoaded]);
+
   if (!fontsLoaded) {
-    return <AppLoading />;
+    return null;
   }
 
   return (
@@ -30,8 +36,10 @@ export const App: React.FC = () => {
 
       <GestureHandlerRootView style={{ flex: 1 }}>
         <SafeAreaProvider>
-          <Home />
-          <Footer />
+          <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+            <Home />
+            <Footer />
+          </View>
         </SafeAreaProvider>
       </GestureHandlerRootView>
     </ThemeProvider>
--- android/app/src/main/jni/MainComponentsRegistry.h
+++ android/app/src/main/jni/MainComponentsRegistry.h
@@ -13,7 +13,7 @@ class MainComponentsRegistry
  public:
   // Adapt it to the package you used for your Java class.
   constexpr static auto kJavaDescriptor =
-      "Lcom/kahsh/newarchitecture/components/MainComponentsRegistry;";
+      "Lcom/kahshmenu/newarchitecture/components/MainComponentsRegistry;";
 
   static void registerNatives();
 
